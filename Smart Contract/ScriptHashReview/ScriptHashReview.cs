ce7e261badedf271f8c7d3dd774eb4a96d8aa5d5
GwendalBrossard <brossard.gwendal@gmail.com>
2018-05-23 18:53:41 +0200

    feat(SmartContract): added 2 triggers: getNumberOfReviewsFrom & getNumberOfReviewsFor | edited the function EditRevieww

--- Smart Contract/ScriptHashReview/ScriptHashReview.cs
+++ Smart Contract/ScriptHashReview/ScriptHashReview.cs
@@ -9,10 +9,12 @@ public class ScriptHashReview : SmartContract
 {
 	//https://peterlinx.github.io/DataTransformationTools/
 
-	//testinvoke cdf4cdad9a6c201b5501125e4c87e02b65a363f2 addReview ['AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y', '55526d13aa05b8c6f69b31028e11618351a68175', '5', 'Very good']
-	//testinvoke 37143884a7ddc4018abe1497e9f7c0147a59a87a getReview ['AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y', 'bddc990a8f560ac78ca2645bc19e08de536f89c9']
-	//testinvoke 55526d13aa05b8c6f69b31028e11618351a68175 getReviewKey ['#\xba\'\x03\xc52c\xe8\xd6\xe5"\xdc2 39\xdc\xd8\xee\xe955526d13aa05b8c6f69b31028e11618351a68175']
-	//testinvoke cdf4cdad9a6c201b5501125e4c87e02b65a363f2 getReviewFrom ['AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y', '1']
+	// testinvoke f6329adf3ad3f0028b2c9ea63a3247ab51710bed addReview ['AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y', '55526d13aa05b8c6f69b31028e11618351a68175', '5', 'Very good']
+	// testinvoke f6329adf3ad3f0028b2c9ea63a3247ab51710bed getReview ['AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y', '55526d13aa05b8c6f69b31028e11618351a68175']   
+	// testinvoke f6329adf3ad3f0028b2c9ea63a3247ab51710bed getNumberOfReviewsFrom ['AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y']
+	// testinvoke f6329adf3ad3f0028b2c9ea63a3247ab51710bed getNumberOfReviewsFor ['55526d13aa05b8c6f69b31028e11618351a68175']
+	// testinvoke f6329adf3ad3f0028b2c9ea63a3247ab51710bed getReviewFromAddress ['AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y', '1']
+
 	public static readonly byte[] contractOwnerAddress = "AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y".ToScriptHash();
 
     // Utilities
@@ -25,8 +27,8 @@ public class ScriptHashReview : SmartContract
 	private static byte[] NumberOfReviewsFromAddressKey(byte[] address) => ("NBREVIEWSFROM".AsByteArray()).Concat(address);
 	private static byte[] NumberOfReviewsForScriptHashKey(byte[] scriptHash) => ("NBREVIEWSFOR".AsByteArray()).Concat(scriptHash);
 
-	private static byte[] ReviewFromAddressKey(byte[] address, byte[] count) => ("REVIEWFROM".AsByteArray()).Concat(address).Concat(count); ////value = ReviewKey(address, scriptHash)
-	private static byte[] ReviewForScriptHashKey(byte[] scriptHash, byte[] count) => ("REVIEWFOR".AsByteArray()).Concat(scriptHash).Concat(count); //value = ReviewKey(address, scriptHash)
+	private static byte[] ReviewFromAddressKey(byte[] address, byte[] count) => ("REVIEWFROM".AsByteArray()).Concat(address).Concat(count);         //value = ReviewKey(address, scriptHash)
+	private static byte[] ReviewForScriptHashKey(byte[] scriptHash, byte[] count) => ("REVIEWFOR".AsByteArray()).Concat(scriptHash).Concat(count);  //value = ReviewKey(address, scriptHash)
 
 
 	public static Object Main(string operation, params object[] args)
@@ -49,8 +51,8 @@ public class ScriptHashReview : SmartContract
 
 				review.reviewOwner = (byte[])args[0];
                 review.scriptHash = (byte[])args[1];
-				review.note = (BigInteger)args[2];
-				review.text = (String)args[3];
+				review.rating = (BigInteger)args[2];
+				review.comment = (String)args[3];
 
 				return AddReview(review);
 			}
@@ -71,8 +73,8 @@ public class ScriptHashReview : SmartContract
 
                 review.reviewOwner = (byte[])args[0];
                 review.scriptHash = (byte[])args[1];
-                review.note = (BigInteger)args[2];
-                review.text = (String)args[3];
+                review.rating = (BigInteger)args[2];
+                review.comment = (String)args[3];
 
                 return EditReview(review);
             }
@@ -82,11 +84,19 @@ public class ScriptHashReview : SmartContract
 			}        
 			if (operation == "getNumberOfReviewsFrom")
 			{
-				
+				if (args.Length != 1) { return false; }
+
+				byte[] address = (byte[])args[0];
+
+				return GetNumberOfReviewsFrom(address);
 			}
 			if (operation == "getNumberOfReviewsFor")
 			{
-				
+				if (args.Length != 1) { return false; }
+                
+                byte[] scriptHash = (byte[])args[0];
+
+				return GetNumberOfReviewsFor(scriptHash);
 			}
 			if (operation == "getReviewFromAddress")
 			{
@@ -115,37 +125,35 @@ public class ScriptHashReview : SmartContract
 	{
 		if (!Runtime.CheckWitness(review.reviewOwner)) { return false; }
 
+		// An address can only post 1 review per scriptHash
 		if (Storage.Get(Context(), ReviewKey(review.reviewOwner, review.scriptHash)).Length > 0) 
 		{
 
 			Runtime.Log("This address has already posted a review for this scripthash");
 			return false; 
 		}
-
-        //Check if the review already exists. If yes, return false because we don't want to add the review to the lists?
-
+      
 		if (!IsReviewOK(review)) { return false; }
 
+        // Store the review
 		byte[] reviewKey = ReviewKey(review.reviewOwner, review.scriptHash);
-		Runtime.Notify("-- Review key --");
-        Runtime.Notify(reviewKey.AsString());
 		byte[] serializedReview = Neo.SmartContract.Framework.Helper.Serialize(review);
 
 		Storage.Put(Context(), reviewKey, serializedReview);
 
-		//Increment the number of reviews from the address
+		// Increment the number of reviews from the address
 		byte[] currentNumberOfReviewsFromAddress = Storage.Get(Context(), NumberOfReviewsFromAddressKey(review.reviewOwner));
 		byte[] newNumberOfReviewsFromAddress = (currentNumberOfReviewsFromAddress.AsBigInteger() + 1).AsByteArray();
 
 		Storage.Put(Context(), NumberOfReviewsFromAddressKey(review.reviewOwner), newNumberOfReviewsFromAddress);
 
-        //Increment the number of review for the scripthash
+        // Increment the number of review for the scriptHash
 		byte[] currentNumberOfReviewsForScriptHash = Storage.Get(Context(), NumberOfReviewsForScriptHashKey(review.scriptHash));
 		byte[] newNumberOfReviewsForScriptHash = (currentNumberOfReviewsForScriptHash.AsBigInteger() + 1).AsByteArray();
 
 		Storage.Put(Context(), NumberOfReviewsForScriptHashKey(review.scriptHash), newNumberOfReviewsForScriptHash);
 
-		//Add the corresponding ReviewKey 
+		// Add the corresponding ReviewKey 
 		Storage.Put(Context(), ReviewFromAddressKey(review.reviewOwner, newNumberOfReviewsFromAddress), reviewKey);
 		Storage.Put(Context(), ReviewForScriptHashKey(review.scriptHash, newNumberOfReviewsForScriptHash), reviewKey);
 
@@ -167,12 +175,28 @@ public class ScriptHashReview : SmartContract
 	public static bool EditReview(Review review)
 	{
 		if (!Runtime.CheckWitness(review.reviewOwner)) { return false; }
+        
+		// Check if the review exists. If no, return false because we don't want to create a review here.
+		byte[] previousSerializedReview = Storage.Get(Context(), ReviewKey(review.reviewOwner, review.scriptHash));
+		if (previousSerializedReview.Length < 1) { return false; }
+        
+		// Check if the caller address is the same as review owner's address
+		Review previousDeserializedReview = (Review)Neo.SmartContract.Framework.Helper.Deserialize(previousSerializedReview);
 
-		//Check if the review exists. If no, return false because we don't want to create a review here.
-		if (Storage.Get(Context(), ReviewKey(review.reviewOwner, review.scriptHash)).Length < 1) { return false; }
+		if (previousDeserializedReview.reviewOwner != review.reviewOwner)
+		{
+			Runtime.Notify("You can only edit your reviews.");
+			return false;
+		}
+
+        // You can only change the rating and the comment of the review
+		if (previousDeserializedReview.reviewOwner != review.reviewOwner || previousDeserializedReview.scriptHash != review.scriptHash)
+		{
+			Runtime.Notify("You can only edit the rating and the comment of the review.");
+            return false;
+		}
 
 		if (!IsReviewOK(review)) { return false; }
-        
         
         byte[] key = ReviewKey(review.reviewOwner, review.scriptHash);
         byte[] serializedReview = Neo.SmartContract.Framework.Helper.Serialize(review);
@@ -184,14 +208,14 @@ public class ScriptHashReview : SmartContract
 
 	public static bool DeleteReview(byte[] address, byte[] scriptHash)
 	{
-		//Check phatansma contract RemoveMessage
+		// TODO
 		if (!Runtime.CheckWitness(address)) { return false; }
 
 		byte[] key = ReviewKey(address, scriptHash);
 
-        //Check if the review exists before deleting it?
+        // Check if the review exists before deleting it?
 
-        //Also delete from list of review for script hash and list of review from address
+        // Also delete from list of review for script hash and list of review from address
 
 		Storage.Delete(Context(), key);
 
@@ -230,11 +254,31 @@ public class ScriptHashReview : SmartContract
         return review;
     }
 
+	public static BigInteger GetNumberOfReviewsFrom(byte[] address)
+	{
+		byte[] key = NumberOfReviewsFromAddressKey(address);
+        byte[] numberOfReviewsFromAddressKey = Storage.Get(Context(), key);
+
+		if (numberOfReviewsFromAddressKey.Length < 1) { return 0; }
+
+		return numberOfReviewsFromAddressKey.AsBigInteger();
+	}
+
+	public static BigInteger GetNumberOfReviewsFor(byte[] scriptHash)
+    {
+		byte[] key = NumberOfReviewsForScriptHashKey(scriptHash);
+		byte[] numberOfReviewsForScriptHashKey = Storage.Get(Context(), key);
+
+		if (numberOfReviewsForScriptHashKey.Length < 1) { return 0; }
+
+		return numberOfReviewsForScriptHashKey.AsBigInteger();
+    }
+
 	public static bool IsReviewOK(Review review)
 	{
-		//Check if the scripthash exists or is a good value, check the length
-		if (review.note < 0 || review.note > 10) { return false; }      
-        if (review.text.Length <= 0) { return false; }
+		// Check if the scripthash exists or is a good value, check the length
+		if (review.rating < 0 || review.rating > 10) { return false; }      
+        if (review.comment.Length <= 0) { return false; }
         
 		return true;
 	}
@@ -244,7 +288,7 @@ public class ScriptHashReview : SmartContract
 	{
 		public byte[] reviewOwner;
 		public byte[] scriptHash;
-		public BigInteger note;
-		public String text;	
+		public BigInteger rating;
+		public String comment;	
 	}
 }
