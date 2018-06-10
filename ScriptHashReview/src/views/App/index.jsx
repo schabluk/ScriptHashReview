ff6a0016f9f0f51992dd52c5a0ea8d1054f046cc
schabluk <lukasz.schabek@gmail.com>
2018-06-10 15:18:59 +0200

    Apply changes to Pages.

--- ScriptHashReview/src/views/App/index.jsx
+++ ScriptHashReview/src/views/App/index.jsx
@@ -1,33 +1,123 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import injectSheet from 'react-jss';
-import { Route, Router, Switch, Link, NavLink } from 'react-router-dom';
-import { Layout } from 'antd'
+import React from 'react'
+import PropTypes from 'prop-types'
+import injectSheet from 'react-jss'
+import { Route, Router, Switch, Link, NavLink } from 'react-router-dom'
+import { Layout, Menu, Card, Rate } from 'antd'
+import Slider from 'react-slick'
 
-import AddReview from './../../components/ScriptHashReviewActions/AddReview';
-import GetReview from './../../components/ScriptHashReviewActions/GetReview';
+import ImageLoader from './../../components/ImageLoader'
 import Home from '../Home'
 import NotFound from '../NotFound'
 
-// import styles from '../../stylesheets/styles.scss';
-// import '../../stylesheets/bootstrap/css/bootstrap.min.css';
 import 'antd/dist/antd.css'
-import '../../stylesheets/index.css';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import '../../stylesheets/index.css'
 
 const { Header, Content } = Layout
 
 class App extends React.Component {
   static propTypes = {
-     history: PropTypes.object.isRequired,
-     service: PropTypes.object.isRequired,
-   }
+    history: PropTypes.object.isRequired,
+    service: PropTypes.object.isRequired,
+  }
+
+  state = {
+    tokens: [],
+    reviews: [],
+    tokenIndex: 0
+  }
+
+  get selectedToken () {
+    return this.state.tokens[this.state.tokenIndex]
+  }
+
+  SliderSettings = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    speed: 350,
+    swipeToSlide: true,
+    focusOnSelect: true,
+    initialSlide: this.state.tokenIndex,
+    afterChange: index => {
+      const { service: { contract: api } } = this.props
+
+      this.setState({tokenIndex: index}, () => {
+        const { hash } = this.selectedToken
+
+        api.getReviews(hash).then(reviews => this.setState({reviews}))
+      })
+    }
+  }
+
+  componentWillMount () {
+    const { service: { contract: api } } = this.props
+
+    api.getTokens().then(tokens => this.setState({tokens})).then(() => {
+      const { hash } = this.selectedToken
+
+      api.getReviews(hash).then(reviews => this.setState({reviews}))
+    })
+  }
 
   // HoC for Router, to pass Service to the pages Component.
   StoreRoute = ({component: Component, ...rest}) => (
     <Route {...rest} render={
-      props => <Component {...props} service={this.props.service} />
+      props => (
+        <Component
+          {...props}
+          service={this.props.service}
+          tokens={this.state.tokens}
+          tokenIndex={this.state.tokenIndex}
+          reviews={this.state.reviews}
+        />
+      )
     } />
   )
+
+  handleMenu = ({key}) => { this.props.history.push(key) }
+
+  renderMenu () {
+    const { history: { location: { pathname } } } = this.props
+
+    return (
+      <Menu
+        style={{ lineHeight: '62px' }}
+        theme='light'
+        mode='horizontal'
+        defaultSelectedKeys={[pathname]}
+        onClick={this.handleMenu}
+      >
+        <Menu.Item key='/'>Home</Menu.Item>
+        <Menu.Item key='/page-a'>My Ratings</Menu.Item>
+        <Menu.Item key='/page-b'>Ecosystem</Menu.Item>
+      </Menu>
+    )
+  }
+
+  renderTokens () {
+    const { tokens } = this.state
+
+    return tokens.map(({hash, name, symbol, supply, image}, key) => {
+      // ToDo: fetch from backend or avg of last ratings.
+      const averageRating = Math.floor(Math.random()*(9-5+1)+5)/2
+
+      return (
+        <Card key={key}
+          bordered={false}
+          className='slide'
+          title={<ImageLoader src={image} alt={name} />}
+          extra={<div className='symbol'>{symbol}</div>}>
+          <div className='name'>{name}</div>
+          <Rate disabled allowHalf defaultValue={averageRating} />
+        </Card>
+      )
+    })
+  }
 
   render () {
     const { StoreRoute } = this
@@ -40,6 +130,9 @@ class App extends React.Component {
             {/* this.renderLogo() */}
             {/* this.renderMenu() */}
           </Header>
+          <Slider {...this.SliderSettings}>
+            { this.renderTokens() }
+          </Slider>
           <Content>
             <Switch>
               <StoreRoute exact path='/' component={Home} />
@@ -59,4 +152,4 @@ const styles = {
   }
 }
 
-export default injectSheet(styles)(App);
+export default injectSheet(styles)(App)
--- ScriptHashReview/src/views/Home.js
+++ ScriptHashReview/src/views/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
+
+import AddReview from './../components/AddReview'
+import GetReview from './../components/GetReview'
 
 import './Home.css'
 
@@ -7,31 +10,33 @@ class Home extends React.Component {
   static displayName = 'Home'
 
   static propTypes = {
-    // service: PropTypes.object.isRequired
+    tokens: PropTypes.array,
+    tokenIndex: PropTypes.number,
+    reviews: PropTypes.array
   }
 
   render () {
+    const { tokens, tokenIndex, reviews } = this.props
+
+    console.log(reviews)
+
     return (
       <div className='home'>
-        <section className='mb-0'>
-          <div className='container'>
-            <h2 className='text-center mt-5 mb-5'>What is ScriptHashReview?</h2>
-            <div className='row'>
-              <div className='col-12'>
-                <p>
-                  ScriptHashReview aims at allowing users to review every smart contract and their
-                  script hash associated, that are made available on the NEO Smart Economy. For each
-                  unique script hash on the NEO Blockchain, every user (public key) can leave a review
-                  with containing a rating as well as a comment associated to it.
-                </p>
-                <p>
-                  Down below, you will find all the functionalities available at the moment that
-                  interact with the blockchain to store and retrieve information from it.
-                </p>
-              </div>
-            </div>
+        <h3 className='jumbotron'>
+          Script Hash Review
+        </h3>
+        <div className='description'>
+          ScriptHashReview aims at allowing users to review every smart contract and their
+          script hash associated, that are made available on the NEO Smart Economy.
+        </div>
+        <div className='media'>
+          <div style={{flex: 1}}>
+            {
+              reviews.map((review, key) => <GetReview key={key} {...review} />)
+            }
           </div>
-        </section>
+          <AddReview tokens={tokens} active={tokenIndex} />
+        </div>
       </div>
     )
   }
